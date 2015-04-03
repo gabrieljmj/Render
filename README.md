7a962503b587bd04d1479a2d076a93274545d686
Gabriel Jacinto <gamjj74@hotmail.com>
2015-04-03 04:40:01 -0300

    Add foreach feature

--- README.md
+++ README.md
@@ -32,3 +32,27 @@ Result:
 Gabriel, 16
 Online
 ```
+
+### Using foreach
+```js
+render
+    .element('your-websites')
+        .setVar('websites', [
+            {name: {company: 'Facebook Inc.'}},
+            {name: {company: 'GitHub'}}
+        ])
+    .render();
+```
+HTML:
+```html
+<div id="your-websites">
+    $foreach(websites as website){
+        <li>${website.company}
+    }
+</div>
+```
+Result:
+```
+• Facebook Inc.
+• GitHub
+```
--- src/render.js
+++ src/render.js
@@ -30,6 +30,16 @@ Element.prototype.getVars = function () {
     return this.vars;
 }
 
+Element.prototype.getVar = function (varName) {
+    for (var k in this.vars) {
+        if (this.vars[k].name === varName) {
+            return this.vars[k].value;
+        }
+    }
+}
+
+
+
 function Render() {
     this.elements = [];
 }
@@ -53,8 +63,43 @@ Render.prototype.setVar = function (name, value) {
 }
 
 Render.prototype.getVars = function (id) {
-    var regexp = /\${([a-zA-Z0-9]+)}/g;
+    return this.getVarsFromContent(document.getElementById(id).innerHTML);
+}
+
+Render.prototype.getLists = function (id) {
+    var regexpForCatchListUsedVar = /\$foreach\(([a-zA-Z0-9. ]+)\)\{\s.*\s+\}/g;
+    var regexpForCatchInside = /\$foreach\(.*\)\{(\s+.*\s+)\}/g;
+    var regexpForCatchCompleteLists = /(\$foreach\(.*\)\{\s.*\s+\})/g;
     var content = document.getElementById(id).innerHTML;
+    var lists = [];
+
+    while((names = regexpForCatchCompleteLists.exec(content)) !== null) {
+        var list = names[1];
+        var listArray = regexpForCatchListUsedVar.exec(list);
+        listArray = listArray[1];
+        var usedVar = listArray.split(' as ');
+        listArray = usedVar[0];
+        usedVar = usedVar[usedVar.length - 1];
+        var listContent = regexpForCatchInside.exec(list);
+        listContent = listContent[1];
+        var data = {array: listArray, var: usedVar, content: listContent, complete: list};
+
+        lists.push(data);
+    }
+
+    return lists;
+}
+
+Render.prototype.getVar = function (elementId, varName) {
+    for (var k in this.elements) {
+        if (this.elements[k].getId() === elementId) {
+            return this.elements[k].getVar(varName);
+        }
+    }
+}
+
+Render.prototype.getVarsFromContent = function (content) {
+    var regexp = /\${([a-zA-Z0-9._\.]+)}/g;
     var result = [];
 
     while ((vars = regexp.exec(content)) != null) {
@@ -67,11 +112,39 @@ Render.prototype.getVars = function (id) {
 Render.prototype.render = function () {
     for (var k in this.elements) {
         var element = this.elements[k];
-        var vars = this.getVars(element.getId());
-        var el = document.getElementById(element.getId());
+        var vars    = this.getVars(element.getId());
+        var elementV = element.getVars();
+        var lists   = this.getLists(element.getId());
+        var el      = document.getElementById(element.getId());
         var content = el.innerHTML;
-        var elVars = element.getVars();
+        var elVars  = element.getVars();
+        var contentList = [];
 
+        for (var l in lists) {
+            var contentVars = this.getVarsFromContent(lists[l].content);
+            for (var y in elementV) {
+                if (lists[l].array === elementV[y].name) {
+                    for (var v in contentVars) {
+                        var objVarSplit = contentVars[v].split('.');
+                        objVar = objVarSplit[0];
+
+                        if (objVar === lists[l].var) {
+                            var listArr = this.getVar(element.getId(), lists[l].array);
+                            originalObj = objVarSplit.join('.');
+                            for (var lv in listArr) {
+                                var newObjVar = objVarSplit;
+                                newObjVar[0] = 'listArr[lv]';
+                                var obj = newObjVar.join('.');
+                                contentList.push(lists[l].content.replace('${' + originalObj + '}', eval(obj)));
+                            }
+                        }
+                    }
+                }
+            }
+
+            content = content.replace(lists[l].complete, contentList.join(''));
+        }
+        
         for (var z in elVars) {
             for (var y in vars) {
                 if (vars[y] === elVars[z].name){
